e097506b6b31936eaecc753f78a5df1dd1cf896a
Keren Villatoro <kerenv@outlook.com>
2024-11-26 21:10:33 -0800

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 //App.jsx
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from "./components/navbar/Navbar";
-import AboutPage from "./pages/AboutPage";
-import BlogPage from "./pages/BlogPage";
-import HomePage from "./pages/HomePage";
-import NewsletterPage from "./pages/NewsletterPage";
+import Navbar from "./components/navbar/Navbar"
+import AboutPage from "./pages/AboutPage"
+import BlogPage from "./pages/BlogPage"
+import HomePage from "./pages/HomePage"
+import NewsletterPage from "./pages/NewsletterPage"
+import { Route,Routes } from 'react-router-dom';
 import { CallbackPage } from "./pages/callback-page";
-import JournalEntry from "./pages/JournalEntryPage";
+import JournalEntry from "./pages/JournalEntryPage"
 
 function App() {
   return (
-    <Router>
-      <div className="mx-auto">
-        <Navbar />
-        <div>
-          <div className="p-10">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/newsletter" element={<NewsletterPage />} />
-              <Route path="/callback" element={<CallbackPage />} />
-              <Route path="/journal/create" element={<JournalEntry />} />
-            </Routes>
-          </div>
+    <div className="mx-auto">
+      <Navbar />
+      <div>
+        <div className="p-10">
+        {/* <JournalPage /> */}
+          <Routes>        
+            <Route path="/"  element={<HomePage/>} />
+            <Route path="/about"  element={<AboutPage />}/>
+            <Route path="/blog" element={<BlogPage/>}/>
+            <Route path="/newsletter"  element={<NewsletterPage/>}/>
+            <Route path="/callback"  element={<CallbackPage/>}/>
+            <Route path="/journal/create"  element={<JournalEntry/>}/>
+          </Routes>
         </div>
       </div>
-    </Router>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
